f6fcb6c437b2ccb086c043c20c58e4ba75afacc7
Grace <glkerr07@gmail.com>
2019-10-26 17:57:29 +0100

    Player Updated

--- player.py
+++ player.py
@@ -1,33 +1,37 @@
 
 class Player:
     def __init__(self, playerName):
-        self.playerName = playerName
+        self.player_name = playerName
         self.health = 50
         self.cannons = 1
         self.moves =[]
-        self.catRelations = None
-        self.dogRelations = None
-        self.mapPos = "0,0"
+        self.cat_relations = None
+        self.dog_relations = None
+        self.map_ros = "0,0"
+        self.inventory = 100
 
-    def updateHealth(self, newHealth):
-        self.health = newHealth
+    def take_damage(self, newHealth):
+        self.health -= newHealth
         return self.health
 
-    def updateCannons(self, newCannons):
+    def update_cannons(self, newCannons):
         self.cannons = newCannons
         return self.cannons
 
-    def updateCatRel(self, newCatRel):
-        self.catRelations = newCatRel
-        return self.catRelations
+    def update_cat_rel(self, newCatRel):
+        self.cat_relations = newCatRel
+        return self.cat_relations
 
-    def updateDogRel(self, newDogRel):
-        self.dogRelations = newDogRel
-        return self.dogRelations
+    def update_dog_rel(self, newDogRel):
+        self.dog_relations = newDogRel
+        return self.dog_relations
 
-    def updatePos(self, newPos):
-        self.mapPos = newPos
-        return self.mapPos
+    def update_pos(self, newPos):
+        self.map_pos = newPos
+        return self.map_pos
 
+    def lose_loot(self, amount_lost):
+        self.inventory -= amount_lost
+        return self.inventory
 
 
