90eb2f73405a522b208dc1ddbe08ab61df569f22
isaac <isaac.s.orr@gmail.com>
2019-10-26 15:55:58 +0100

    Added island class

---
+++ Island.py
@@ -0,0 +1,27 @@
+import random
+class Island:
+    alignment = 0
+    timeUntilNextShip = 0
+
+    def create(self):
+        alignment = random.randint(1, 4)
+        timeUntilNextShip = 15
+
+        if alignment == 1:
+            #neutral
+            alignment = 'N'
+        elif alignment == 2:
+            #cat
+            alignment = 'C'
+        elif alignment == 3:
+            #dog
+            alignment = 'D'
+        elif alignment == 4
+            #allied
+            alignment = 'A'
+
+    def __repr__(self):
+        return(alignment)
+
+
+
--- venv/Scripts/Activate.ps1
+++ venv/Scripts/Activate.ps1
@@ -27,7 +27,7 @@ function global:deactivate ([switch]$NonDestructive) {
 
 deactivate -nondestructive
 
-$env:VIRTUAL_ENV="C:\Users\Jacob\Documents\HackTheMidlands2019\venv"
+$env:VIRTUAL_ENV="C:\Users\isaac\PycharmProjects\HackTheMidlands2019\venv"
 
 if (! $env:VIRTUAL_ENV_DISABLE_PROMPT) {
     # Set the prompt to include the env name
