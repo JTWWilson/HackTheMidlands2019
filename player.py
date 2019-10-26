a66726e0f883a4a24bfe05553921ad602cdef3b1
aidandyde <atdyde@gmail.com>
2019-10-26 22:38:23 +0100

    We have an octocat now!

--- player.py
+++ player.py
@@ -7,7 +7,7 @@ class Player:
         self.moves =[]
         self.cat_relations = None
         self.dog_relations = None
-        self.map_pos = (0,0)
+        self.map_pos = (0, 0)
         self.inventory = 100
 
     def take_damage(self, newHealth):
