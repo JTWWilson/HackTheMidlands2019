4beb5d80cf539de5dfb5865bbdbd781aae251f17
Jacob <jacobtwwilson@gmail.com>
2019-10-26 20:06:59 +0100

    Change formatting and fix missing self args

--- ship.py
+++ ship.py
@@ -1,23 +1,23 @@
 # health, cannons, animal_type
-class ship:
-	def __init__(self, health, cannons, animal_type):
-		self.health = health
-		self.cannons = cannons
-		self.animal_type = animal_type
+class Ship:
+    def __init__(self, health, cannons, animal_type):
+        self.health = health
+        self.cannons = cannons
+        self.animal_type = animal_type
 
-	def update_cannons(self, newCannons):
+    def update_cannons(self, newCannons):
         self.cannons = newCannons
         return self.cannons
 
-    def take_damage(hit_points):
-    	self.health -= hit-points
+    def take_damage(self, hit_points):
+        self.health -= hit-points
 
-    def get_health():
-    	return self.health
+    def get_health(self):
+        return self.health
 
-    def destroy():
-    	print("ship destroyed")
+    def destroy(self):
+        print("ship destroyed")
 
-    def get_animal_type():
-    	return self.animal_type
+    def get_animal_type(self):
+        return self.animal_type
 
