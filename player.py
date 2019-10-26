68751b060ecc7178fe183f1ead1ed3f149c00614
Grace <glkerr07@gmail.com>
2019-10-26 17:36:15 +0100

    test

---
+++ player.py
@@ -0,0 +1,15 @@
+
+class Player:
+    def __init__(self, playerName):
+        self.playerName = playerName
+        self.health = 50
+        self.cannons = 1
+        self.moves =[]
+        self.catRelations = None
+        self.dogRelations = None
+        self.mapPos = "0,0"
+
+
+
+
+
